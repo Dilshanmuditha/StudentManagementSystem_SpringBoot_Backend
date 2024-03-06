6431a00d9584609526f263e088549ea27bf3c4c6
L.D.M.Kumara <mudithadilshan1999@gmail.com>
2024-03-07 00:12:05 +0530

    initialization Admin register and update

---
+++ src/main/java/com/example/studentmanagement/model/Admin.java
@@ -0,0 +1,54 @@
+package com.example.studentmanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    private int id;
+    private String name,email,password,image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
