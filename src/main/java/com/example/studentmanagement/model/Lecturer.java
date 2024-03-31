8f49264f02f873729fa1b6ecf9e2321f188e54db
L.D.M.Kumara <mudithadilshan1999@gmail.com>
2024-03-31 10:50:13 +0530

    initialization Admin register and update

---
+++ src/main/java/com/example/studentmanagement/model/Lecturer.java
@@ -0,0 +1,92 @@
+package com.example.studentmanagement.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "lecturer")
+public class Lecturer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nic;
+    private String name;
+    private String email;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String address;
+    private String mobile;
+    private String userName;
+    private String password;
+}
