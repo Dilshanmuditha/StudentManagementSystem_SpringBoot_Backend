19fafb12f29fb5141a94679a767805baf4ee2551
hashan <nrmhckarunasena@students.nsbm.ac.lk>
2024-04-27 00:08:28 +0530

    hashan

--- src/main/java/com/example/studentmanagement/repository/StudentRepository.java
+++ src/main/java/com/example/studentmanagement/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.example.studentmanagement.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Student findByUserName(String userName);
 }
--- src/main/java/com/example/studentmanagement/service/LectureServiceImpl.java
+++ src/main/java/com/example/studentmanagement/service/LectureServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.studentmanagement.service;
 
+import com.example.studentmanagement.model.Admin;
 import com.example.studentmanagement.model.Lecturer;
 import com.example.studentmanagement.repository.LecturerRepository;
+import com.example.studentmanagement.security.JwtTokenService;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +16,9 @@ import java.util.Optional;
 public class LectureServiceImpl implements LectureService{
     @Autowired
     LecturerRepository lecturerRepository;
+    @Autowired
+    JwtTokenService tokenService;
+
     public Lecturer save(Lecturer lecturer){return lecturerRepository.save(lecturer);}
     public List<Lecturer> getLecturer(){return lecturerRepository.findAll();}
 
@@ -33,7 +40,19 @@ public class LectureServiceImpl implements LectureService{
         return lecturerRepository.save(existingLecturer);
     }
 
-    public Optional<Lecturer> view(Integer id) {
-        return lecturerRepository.findById(id);
+    public Optional<Lecturer> view(Integer id) {return lecturerRepository.findById(id);
+    }
+
+
+    public ResponseEntity<?> login(String userName, String password) {
+        Lecturer lecturer = lecturerRepository.findByUserName( userName);
+        if (lecturer != null && lecturer.getPassword().equals(password)) {
+            // Authentication successful, generate JWT token
+//            String token = jwtTokenService.generateToken(email);
+            return ResponseEntity.ok(lecturer);
+        } else {
+            // Authentication failed
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid UserName or password");
+        }
     }
 }
